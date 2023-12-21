ed8b08badca6459bde4ad3a917097351a2d9deb9
ThienVuVan <>
2023-12-22 01:28:44 +0700

    add Distributed Tracing and Apache Kafka

---
+++ notification-service/src/main/java/com/vvt/NotificationServiceApplication.java
@@ -0,0 +1,20 @@
+package com.vvt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+       SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent){
+        // send out email in here
+        log.info("notification from order " + orderPlacedEvent.getOrderNumber());
+    }
+}
--- order-service/src/main/java/com/vvt/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/vvt/orderservice/service/OrderService.java
@@ -3,11 +3,13 @@ package com.vvt.orderservice.service;
 import com.vvt.orderservice.dto.InventoryResponse;
 import com.vvt.orderservice.dto.OrderItemsDto;
 import com.vvt.orderservice.dto.OrderRequest;
+import com.vvt.orderservice.event.OrderPlacedEvent;
 import com.vvt.orderservice.model.Order;
 import com.vvt.orderservice.model.OrderItems;
 import com.vvt.orderservice.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -23,6 +25,7 @@ import java.util.stream.Collectors;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     public Boolean placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -47,6 +50,7 @@ public class OrderService {
         Boolean check = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::getIsInStocks);
         if(check){
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return true;
         }else{
             return false;
